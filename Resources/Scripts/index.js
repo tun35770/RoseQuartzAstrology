fbcd26bf2de89f3a3e19a9ba40c11d06876a70ab
tun35770 <tun35770@temple.edu>
2021-12-15 21:57:32 -0500

    Working Birthdate form

--- Resources/Scripts/index.js
+++ Resources/Scripts/index.js
@@ -3,37 +3,64 @@ const myParent = document.body;
 initializeBirthdateSelect();
 
 function initializeBirthdateSelect(){
-    const monthList = document.getElementsByName("month");
-    const dayList = document.getElementsByName("day");
-    const yearList = document.getElementsByName("year");
+
+    //create selects
+    const monthList = document.getElementById("month");
     
+    const dayList = document.getElementById("day");
+
+    const yearList = document.getElementById("year");
+    
+
+    //arrays containing option values
     const months = ["January", "February", "March", "April", "May", "June", "July"
                     , "August", "September", "October", "November", "December"];
-    const days = new Array(31);
+    const days = new Array(32);
     const years = new Array(100);
 
+    /* let monthTitleOption = document.createElement("option");
+    monthTitleOption.value = "Month";
+    monthTitleOption.text = "Month";
+
+    let dayTitleOption = document.createElement("option");
+    dayTitleOption.value = "Day";
+    dayTitleOption.text = "Day";
+
+    let yearTitleOption = document.createElement("option");
+    yearTitleOption.value = "Year";
+    yearTitleOption.text = "Year"; */
+
     //initialize days[] with 1-31
-    for(let i = 1; i <= 31; i++){
+    for(let i = 0; i <= 31; i++){
         days[i] = i;
     }
 
     //initialize years[] with 1922-2021
     for(let i = 0; i < 100; i++){
-        years[i] = i + 1922;
+        years[i] = 2021 - i;
     }
 
-    //create option elements
-    let monthOption = document.createElement("option");
-    let dayOption = document.createElement("option");
-    let yearOption = document.createElement("option");
+    //add months as options to monthList
+    for(let i = 0; i < months.length; i++){
+        let option = document.createElement("option");
+        option.value = months[i];
+        option.text = months[i];
+        monthList.appendChild(option);
+    }
 
-    //set their values
-    monthOption.value = months;
-    dayOption.value = days;
-    yearOption.value = years;
+    //add days as options to dayList
+    for(let i = 0; i < days.length; i++){
+        let option = document.createElement("option");
+        option.value = days[i];
+        option.text = days[i];
+        dayList.appendChild(option);
+    }
 
-    //append them to respective selects
-    monthList.appendChild(monthOption);
-    dayList.appendChild(dayOption);
-    yearList.appendChild(yearOption);
+    //add years as options to yearList
+    for(let i = 0; i < years.length; i++){
+        let option = document.createElement("option");
+        option.value = years[i];
+        option.text = years[i];
+        yearList.appendChild(option);
+    }
 }
